2bbc460ccbcd6e2975af26353149d5e51b1bb6ba
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2018-11-25 21:57:22 +0200

    Hastane Uygulaması
    İçine hasta ekleyebileceğimiz bir otomasyon oluşturduk.

---
+++ Hastane Uygulaması/src/Hastane/Hasta.java
@@ -0,0 +1,12 @@
+package Hastane;
+
+public class Hasta {
+   
+    String isim;
+    String hastalık; 
+ 
+    public Hasta(String isim,String hastalık) {
+        this.isim = isim;
+        this.hastalık = hastalık;
+    }
+}
---
+++ Hastane Uygulaması/src/Hastane/Hastane.java
@@ -0,0 +1,39 @@
+package Hastane;
+
+import java.util.Scanner;
+
+public class Hastane {
+    // bu da bir hastane sınıfı. hastaları burda tutacağız.
+    // kaydı da burada yapacağız
+    
+    Scanner klavye = new Scanner(System.in);
+    int kapasite; // hastane kapasitesi
+    int sira = 0; // dizinin sırası
+    Hasta [] hastalar; // hastaları burada tutacağız.
+
+    public Hastane(int kapasite) {
+        this.kapasite = kapasite;
+        this.hastalar = new Hasta[kapasite];
+
+    }
+    
+    public void hastaEkle(Hasta h){
+        if(this.sira == this.hastalar.length) // limit dolduysa
+        {
+            System.out.println("Hastane dolu");
+        }
+        else
+        {
+            this.hastalar[this.sira] = h;
+            this.sira++;
+            System.out.println("Hasta eklendi adı : "+h.isim);
+            System.out.println("Hastalığı : "+h.hastalık);
+        }
+    }
+
+    public static void main(String[] args) {
+        Hasta hastaKisi = new Hasta("Hüseyin","grip"); 
+        Hastane LokmanHekimHastanesi = new Hastane(10);
+        LokmanHekimHastanesi.hastaEkle(hastaKisi);
+    }
+}

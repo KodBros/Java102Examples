b4578d0c7eaa54d4d07f29ce32f0be01533dafc7
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2018-11-27 15:07:37 +0200

    JFrame ile ip adresi uygulaması

---
+++ IPAddress/src/yeni/Yeni.java
@@ -0,0 +1,8 @@
+package yeni;
+import java.net.UnknownHostException; 
+public class Yeni { 
+    public static void main(String[] args) throws UnknownHostException {
+        hostDesktop a = new hostDesktop();
+        a.show();
+    }
+} 
